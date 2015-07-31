35786c3abbcf19eee022ef426412b85d060a7b61
Francis Alonso Gonzalez Zarate <francis_gz@hotmail.com>
2015-07-31 11:21:12 -0500

    hasta video 14, se agrego el movimiento del personaje

--- js/heroe.js
+++ js/heroe.js
@@ -6,25 +6,50 @@ function	Heroe(){
 	this.setHeight(70);
 	this.vx = 15;
 	this.vy = 0;
+	this.limiteDer = 0;
+	this.limiteTope = 0;
 	this.direccion = 1;
-	this.contador = 0
+	this.contador = 0;
+	this.setFill('red');
 	this.caminar = function()
 	{
-
+		this.move(this.vx,0);
+		if (this.getX() > this.limiteDer) 
+			{
+				this.move(this.limiteDer - this.getX() , 0 );
+			}
 	}
 	this.retroceder =  function()
 	{
-
+		this.move(-15,0);
+		if (this.getX() < 0) 
+			{
+				this.move(- this.getX() , 0 );
+			}
 	}
 
 	this.saltar = function()
 	{
 
+		this.vy = -20;
+		this.contador++;
+
 	}
 
-	this.aplicarGravedad = function()
+	this.aplicarGravedad = function(gravedad, vRebote)
 	{
+		this.vy += gravedad;
+		this.move(0,this.vy);
 		
+		if ((this.getY() + this.getHeight()) > this.limiteTope) 
+			{
+				this.setY(this.limiteTope - this.getHeight());
+				this.vy = 0;
+				
+				this.contador = 0; //Para poder saltar de nuevo al terminar de aplicar la gravedad	
+			}
+
+
 	} 
 }    
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-var stage;
+var stage, fondo;
 var keyboard = {};
-
+var intv;
+var personaje;
+var grav = 0.8;
+var val_reb = 0;
 
 
 stage = new Kinetic.Stage({
@@ -9,6 +12,38 @@ stage = new Kinetic.Stage({
 	height: 500,
 	
 });  
+
+function nivelUno(){
+	fondo = new Kinetic.Layer();
+	personaje = new Heroe();
+	personaje.setX(0);
+	personaje.setY(stage.getHeight()-personaje.getHeight());//Se resta al alto del canvas el alto del personaje
+	personaje.limiteDer = stage.getWidth()-personaje.getWidth();
+	personaje.limiteTope = stage.getHeight();
+	fondo.add(personaje);
+	stage.add(fondo);
+}
+
+
+
+
+function moverPersonaje()
+{
+
+	if (keyboard[37]) 
+		{
+			personaje.retroceder();
+		}
+	if(keyboard[39])
+	{
+		personaje.caminar();
+	}	
+	if(keyboard[38] && personaje.contador < 1)
+	{
+		personaje.saltar();
+	}	
+}
+
 
 
 function addKeyBoardEvents(){
@@ -43,7 +78,7 @@ function hit(a,b){
 	var hit = false;
 
 	//colisiones horizontales
-	if(b.getX() + b.getWidth() >= a.getX()  && b.getX()) < a.getX() + a.getWidth())
+	if(b.getX() + b.getWidth() >= a.getX()  && b.getX() < a.getX() + a.getWidth())
 		{
 			//colisiones verticales
 			if(b.getY() + b.getHeight() >= a.getY() && b.getY() < a.getY() + a.getHeight())
@@ -51,7 +86,7 @@ function hit(a,b){
 		}
 
 	//colisiones de a con b
-		if(b.getX() <= a.getX() && b.getX()) + b.getWidth() >= a.getX() + a.getWidth() )
+		if(b.getX() <= a.getX() && b.getX() + b.getWidth() >= a.getX() + a.getWidth() )
 		{
 			
 			if(b.getY() <= a.getY() &&  b.getY() + 	b.getHeight() >= a.getY() + a.getHeight())
@@ -59,7 +94,7 @@ function hit(a,b){
 		}
 
 	//Colision b con a
-		if(a.getX() <= b.getX() && a.getX()) + a.getWidth() >= b.getX() + b.getWidth() )
+		if(a.getX() <= b.getX() && a.getX() + a.getWidth() >= b.getX() + b.getWidth() )
 		{
 			
 			if(a.getY() <= b.getY() &&  a.getY() + 	a.getHeight() >= b.getY() + b.getHeight())
@@ -68,3 +103,21 @@ function hit(a,b){
 
 	return hit;	
 } 
+
+function aplicarFuerzas()
+{
+	personaje.aplicarGravedad(grav, val_reb);
+}
+
+addKeyBoardEvents();
+nivelUno();
+
+
+intv = setInterval(frameLoop, 1000/20);
+
+function frameLoop ()
+{
+	aplicarFuerzas();
+	moverPersonaje();
+	stage.draw();
+}
